0efe84b57929eec669e45f0298416567e647eed1
dyoogo <78140396+dyoogo@users.noreply.github.com>
2023-03-16 22:00:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
+
+
+// ---- Open and close sidebar ----
+menuBtn.addEventListener('click', evt => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', evt => {
+    sideMenu.style.display = 'none';
+})
+
+// ---- Dark and light mode --------
+themeToggler.addEventListener('click', evt => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
